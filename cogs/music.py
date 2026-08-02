0129fccb5e8d5a035aeda3f6ca943c563e3475e4
Valentraum <kovtunvv13@gmail.com>
2026-08-02 14:44:10 +0200

    Add files via upload

--- cogs/music.py
+++ cogs/music.py
@@ -53,8 +53,8 @@ YDL_OPTS = {
 }
 
 FFMPEG_OPTS = {
-    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
-    "options": "-vn",
+    "before_options": "-nostdin",
+    "options": "-vn -threads 1",
 }
 
 
